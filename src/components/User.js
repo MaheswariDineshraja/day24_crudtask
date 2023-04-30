debcfcaa7b2d9969c174ec49fdf4ef47776e95bb
MaheswariDineshraja <mageshbe2003@yahoo.co.in>
2023-04-30 23:06:12 +0800

    task

---
+++ src/components/User.js
@@ -0,0 +1,22 @@
+
+
+const User = [ 
+{
+
+    id:"1",
+    Name:"Dinesh",
+    Age:"38"
+},{
+
+    id:"2",
+    Name:"Bala",
+    Age:"30"
+},{
+
+    id:"3",
+    Name:"Ravi",
+    Age:"20"
+}
+]
+
+export default User;
